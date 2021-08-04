06bb634a97e210266785cb0fe305478ea3df4ed6
aozora0000 <aozora0000@gmail.com>
2021-08-04 16:55:45 +0900

    AES256bit暗号化復号化関数を実装 #2

--- pool_test.go
+++ pool_test.go
@@ -11,6 +11,7 @@ func TestPool_Get(t *testing.T) {
 		name   string
 		params map[string]string
 		envs   map[string]string
+		args   []string
 		want   map[string]string
 	}{
 		{
@@ -22,7 +23,9 @@ func TestPool_Get(t *testing.T) {
 				"TEST_ENV":        `echo "%TEST_PARAM"`,
 				"TEST_ENV_RESULT": `echo "%TEST_ENV"`,
 			},
+			args: []string{},
 			want: map[string]string{
+				"ARGS":            "",
 				"TEST_PARAM":      "test",
 				"TEST_ENV":        "test",
 				"TEST_ENV_RESULT": "test",
@@ -32,12 +35,19 @@ func TestPool_Get(t *testing.T) {
 			name:   "empty test",
 			params: nil,
 			envs:   nil,
-			want:   map[string]string{},
+			want:   map[string]string{"ARGS": ""},
+		},
+		{
+			name:   "argument test",
+			params: nil,
+			envs:   nil,
+			args:   []string{"test"},
+			want:   map[string]string{"0": "test", "ARGS": "test"},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := NewPool(test.params, test.envs, nil).Init().Get()
+			got := NewPool(test.params, test.envs, test.args).Init().Get()
 			fmt.Println(got)
 			assert.Equal(t, test.want, got)
 			t.Logf("want: %v", test.want)
@@ -52,6 +62,7 @@ func TestPool_Replace(t *testing.T) {
 		command string
 		params  map[string]string
 		envs    map[string]string
+		args    []string
 		want    string
 	}{
 		{
@@ -64,12 +75,21 @@ func TestPool_Replace(t *testing.T) {
 				"TEST_ENV":        `echo "%TEST_PARAM"`,
 				"TEST_ENV_RESULT": `echo "%TEST_ENV"`,
 			},
+			args: []string{},
 			want: "echo \"test\"",
+		},
+		{
+			name:    "empty test",
+			command: "echo \"%0\"",
+			params:  nil,
+			envs:    nil,
+			args:    []string{"test"},
+			want:    "echo \"test\"",
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := NewPool(test.params, test.envs, nil).Init().Replace(test.command)
+			got := NewPool(test.params, test.envs, test.args).Init().Replace(test.command)
 			assert.Equal(t, test.want, got)
 			t.Logf("want: %v", test.want)
 			t.Logf("got: %v", got)
